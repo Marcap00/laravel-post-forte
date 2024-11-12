5ab91d06ae5b19c245710abb3279e934166b5274
GiorgioDordo <djole6789@gmail.com>
2024-11-12 16:58:13 +0100

    fix

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Admin;
 
 use App\Models\Post;
 use App\Http\Controllers\Controller;
-use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -14,8 +13,6 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
-        return view('posts.index', compact('posts'));
         $posts = Post::all();
         // dd($trains);
         return view('posts.index', compact('posts'));
