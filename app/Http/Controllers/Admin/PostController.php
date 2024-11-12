232be812fffdf106383161e4eb0b4be7324d9ebb
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-11-12 16:25:07 +0100

    updated CRUD (create/store/edit/update)

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Http\Requests\StorePostRequest;
+use App\Http\Requests\UpdatePostRequest;
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -20,15 +23,17 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        return view("posts.create");
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StorePostRequest $request)
     {
-        //
+        $data = $request->validate();
+        $post = Post::create($data);
+        return redirect()->route("posts.show", ["id" => $post->id]);
     }
 
     /**
@@ -44,15 +49,20 @@ class PostController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $post = Post::findOrFail($id);
+        return view('posts.edit', compact('pokemon'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdatePostRequest $request, string $id)
     {
-        //
+
+        $data = $request->validate();
+        $post = Post::findOrFail($id);
+        $post->update($data);
+        return redirect()->route("posts.show", ["id" => $post->id]);
     }
 
     /**
