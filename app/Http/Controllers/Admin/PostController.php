c7fc6b71facc54e6caf25dde4f86557904aa3432
GiorgioDordo <djole6789@gmail.com>
2024-11-12 16:37:04 +0100

    createed view index show and create

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\Post;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -13,6 +14,9 @@ class PostController extends Controller
     public function index()
     {
         //
+        $posts = Post::all();
+        // dd($trains);
+        return view('posts.index', compact('posts'));
     }
 
     /**
