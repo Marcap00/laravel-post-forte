134b135eaf9516c9e4e40449f624e802b28f726d
armandotruglio <armando.truglio1@gmail.com>
2024-11-12 16:23:15 +0100

    Added UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,16 +2,26 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Faker\Generator as Faker;
 
 class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+
+        for ($i=0; $i<10; $i++) {
+            $user = new User();
+            $user->name = $faker->name();
+            $user->email = $faker->email();
+            $user->password = Hash::make($faker->password());
+            $user->save();
+        }
     }
 }
